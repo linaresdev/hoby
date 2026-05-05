e6ddbf417225a8afcae68f0c5b9d9b77026d5021
Ramon A <rlinaresdev@gmail.com>
2026-05-05 10:09:22 -0400

    Actualizaciones en proceso

--- App/Providers/HobyServiceProvider.php
+++ App/Providers/HobyServiceProvider.php
@@ -8,11 +8,10 @@ class HobyServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-
+        $this->mergeConfigFrom(__DIR__ . "/../Config/app.php", "hoby");
     }
 
     public function register(): void
     {
-
     }
 }
